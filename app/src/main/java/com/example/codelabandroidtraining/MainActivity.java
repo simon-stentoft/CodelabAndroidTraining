7cd27ae8ff87c10c11dc8390111f6c0a7cc36b6d
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-04-11 20:05:22 +0200

    e

--- app/src/main/java/com/example/codelabandroidtraining/MainActivity.java
+++ app/src/main/java/com/example/codelabandroidtraining/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity", "Hello World");
+        Log.d("MainActivity", "Happy Birthday, Andreas");
     }
 }
